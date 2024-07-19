4d72519da8e4bbf41f91c69c1992dffa534df9e3
Achhuta Nand Jha <achhutanandj@gmail.com>
2024-07-19 17:28:57 +0530

    Delete 4. DECISION STATEMENTS directory

--- 4. DECISION STATEMENTS/pro4.2.py
+++
@@ -1,5 +0,0 @@
-# Write a programthat prompts a user to enter two integer values. Print the message ‘Equals’ if both the entered values are equal.
-num1=eval(input("Enter the first value "))
-num2=eval(input("Enter the second value"))
-if num1-num2==0:
- print("Both the number entered are equal")
