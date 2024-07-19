c9e3d2f9557781c3a8dab9c22b4ce593cf2435ad
Achhuta Nand Jha <achhutanandj@gmail.com>
2024-07-19 17:28:46 +0530

    Delete 3. OPERATORS AND EXPRESSIONS directory

--- 3. OPERATORS AND EXPRESSIONS/pro3.14.py
+++
@@ -1,7 +0,0 @@
-# Write a program to shift input data by 2 bits towards the right.
-
-n=eval(input("Write the input Data n is to we shifted By 2 bits is = "))
-
-y=n/2**2
-print("shift input data by 2 bits towards the right is = " , y)
-
