90a2d065474189c53ea3dcf87a469d6476b94fc7
Achhuta Nand Jha <achhutanandj@gmail.com>
2024-07-19 16:55:21 +0530

    Add files via upload

---
+++ python/problem solving with python/3. OPERATORS AND EXPRESSIONS/3. OPERATORS AND EXPRESSIONS/pro3.8.py
@@ -0,0 +1,17 @@
+# Write a program to display the following table.
+#  X      Y      X**Y 
+# 10      2      100
+# 10      3      1000
+# 10      4      10000
+# 10      5      100000
+
+print("X**Y")
+print("10   \t   2    \t  ",10**2)
+print("10   \t   3    \t  ",10**3)
+print("10   \t   4    \t  ",10**4)
+print("10   \t   5    \t  ",10**5)
+sum=10**2
+print("the square is:",sum)
+print("10                                                   \t           2                                              \t ",10**2)
+
+
