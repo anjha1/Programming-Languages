a4d1067a9b2d2bbb9a4e0e25867ebda6a2f5b172
Achhuta Nand Jha <achhutanandj@gmail.com>
2024-07-19 17:05:09 +0530

    Delete python/problem solving with python/6. FUNCTION directory

--- python/problem solving with python/6. FUNCTION/pro6.1.py
+++
@@ -1,7 +0,0 @@
-"""Write a program to create a function having a name, display. Print the 
-message, “Welcome to Python Programming” inside the function"""
-
-
-def display():
-    print("welcome to python programming")
-display()
