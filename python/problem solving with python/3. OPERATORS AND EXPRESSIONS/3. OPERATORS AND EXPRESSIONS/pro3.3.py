e2a8a1d1c86d1fdb055a0f7a469c23a213d9bce3
Achhuta Nand Jha <achhutanandj@gmail.com>
2024-07-19 16:56:02 +0530

    Delete python/problem solving with python/3. OPERATORS AND EXPRESSIONS/3. OPERATORS AND EXPRESSIONS directory

--- python/problem solving with python/3. OPERATORS AND EXPRESSIONS/3. OPERATORS AND EXPRESSIONS/pro3.3.py
+++
@@ -1,8 +0,0 @@
-# Write a program to calculate simple interest (SI).Read the principle, rate of interest and number of years from the user
-#si = p*r*year/100
-
-p=eval(input("Enter the Principle Amount= "))
-r=eval(input("Enter the Rate of interest= "))
-year=int(input("Enter the Year= "))
-SI=int(p*r*year/100)
-print("Simple Interest " , SI)
