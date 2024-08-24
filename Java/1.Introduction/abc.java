60fd30857f4bc52cbb7d9636dadccf0eadd22fec
Achhuta Nand Jha <achhutanandj@gmail.com>
2024-08-24 14:28:45 +0530

    Add files via upload

---
+++ Java/1.Introduction/abc.java
@@ -0,0 +1,12 @@
+public class abc {
+    public static void main(String[] args) {
+        int x =10;
+        int y = 20;
+        int c =x+y;
+        System.out.println(x + "+" + y + "=" + c);
+        System.out.println(x+y+""+x+y);
+        System.out.println(x+y+"");
+        System.out.println(""+x+y);
+        System.out.println(""+(x+y));
+    }
+}
