b9b789692f6248b041eca1ac448386b8497e7af1
Achhuta Nand Jha <achhutanandj@gmail.com>
2024-09-02 21:23:10 +0530

    Add files via upload

---
+++ Java/5.Arrays in Java/jagged/array1.java
@@ -0,0 +1,20 @@
+public class array1 {
+    public static void main(String[] args) {
+        int nums[][] = {{1,5,10,15,99,90,77},{2,4,6,8},{3,6,9,12}};
+        int row = nums.length; 
+        int col = nums[0].length;
+        int col1 = nums[1].length;
+        int col2 = nums[2].length; 
+
+        for (int i=0;i<row;i++){
+
+        for (int j=0;j<col1;j++){
+            System.out.print(nums[i][j] + " ");
+        }
+        
+        System.out.println(" ");
+        }
+        
+        
+    }
+}
---
+++ Java/5.Arrays in Java/jagged/array2.java
@@ -0,0 +1,17 @@
+public class array2 {
+    public static void main(String[] args) {
+        int nums[][] = {{1,5,10,15,99,90,77},{2,4,6,8},{3,6,9,12}};
+        int row = nums.length; 
+
+        for (int i=0;i<row;i++){
+
+        for (int j=0;j<nums[i].length;j++){
+            System.out.print(nums[i][j] + " ");
+        }
+        
+        System.out.println(" ");
+        }
+        
+        
+    }
+}
